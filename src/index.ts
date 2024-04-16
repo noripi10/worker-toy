3c42da1d7f5963113100914931058d55ffe19eca
sugiyama <sgym.snk@gmail.com>
2024-04-16 11:56:24 +0900

    update binding KV & R2

--- src/index.ts
+++ src/index.ts
@@ -12,13 +12,13 @@ export interface Env {
 	MY_VARIABLE: string;
 
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
+	MY_WORKER_TOY: KVNamespace;
 	//
 	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
 	// MY_DURABLE_OBJECT: DurableObjectNamespace;
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+	MY_PICTURES_BUCKET: R2Bucket;
 	//
 	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
 	// MY_SERVICE: Fetcher;
@@ -37,10 +37,11 @@ const triggerHandler = async (controller: ScheduledController, env: Env) => {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const path = new URL(request.url);
-		console.info({ path });
+		const url = new URL(request.url);
+		console.info({ url: url.pathname });
 
-		if (path.pathname === '/') {
+		// ## Home (HTML + Enviroment Variable)
+		if (url.pathname === '/') {
 			return new Response(
 				`
 <html>
@@ -58,15 +59,110 @@ export default {
 			);
 		}
 
-		if (path.pathname === '/404') {
-			return new Response('Not Found', { status: 404 });
-		}
-
-		if (path.pathname === '/json') {
+		// ## JSON
+		if (url.pathname === '/json') {
 			return Response.json({ key: 'value' });
 		}
 
-		return new Response();
+		// ## KVNamesapce
+		if (url.pathname.startsWith('/kv-list')) {
+			const result = await env.MY_WORKER_TOY.list();
+			const list = result;
+			return Response.json({ list });
+		}
+		if (url.pathname.startsWith('/kv')) {
+			// KeyはQueryから取得
+			const key = url.pathname.split('/').slice(2).join('/');
+			switch (request.method) {
+				case 'GET':
+					const getResult = await env.MY_WORKER_TOY.get(key);
+					return new Response(`KV Value ${key}:${getResult ?? 'null'}}`, { status: 200 });
+
+				case 'POST':
+					const fd = await request.formData();
+					const value = fd.get('value') as string;
+					if (key && value) {
+						await env.MY_WORKER_TOY.put(key, value);
+						return new Response(`KV Upload Success! ${key}:${value}}`, { status: 200 });
+					}
+					break;
+
+				case 'DELETE':
+					if (key) {
+						await env.MY_WORKER_TOY.delete(key);
+						return new Response(`KV Delete Success! ${key}`, { status: 200 });
+					}
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		// ## R2 - Picture
+		if (url.pathname.startsWith('/r2-list')) {
+			const result = await env.MY_PICTURES_BUCKET.list();
+			const list = result.objects.map((e) => ({ key: e.key, size: e.size }));
+			return Response.json({ list });
+		}
+		if (url.pathname.startsWith('/r2')) {
+			// KeyはQueryから取得
+			const key = url.pathname.split('/').slice(2).join('/');
+			console.info('key', key);
+
+			switch (request.method) {
+				case 'GET':
+					const object = await env.MY_PICTURES_BUCKET.get(key ?? '');
+					if (object) {
+						const headers = new Headers();
+						object.writeHttpMetadata(headers);
+						headers.set('etag', object.httpEtag);
+
+						return new Response(object.body, { headers });
+					}
+					break;
+
+				case 'PUT':
+					if (request.body) {
+						const result = await env.MY_PICTURES_BUCKET.put(key, request.body as ReadableStream<any>);
+						return new Response(`Upload Success! ${result?.key} ${result?.size}}`, { status: 200 });
+					}
+					break;
+
+				case 'POST':
+					const fd = await request.formData();
+					const pictures = fd.getAll('pictures').filter((e) => e instanceof File);
+
+					if (pictures) {
+						// const result = await env.MY_PICTURES_BUCKET.put(key, file);
+						console.info({ pictures });
+
+						await Promise.allSettled(
+							pictures.map(async (picture) => {
+								if (picture instanceof File) {
+									const buffer = await picture.arrayBuffer();
+									const result = await env.MY_PICTURES_BUCKET.put(picture.name, buffer);
+									if (request) return result;
+								}
+								return null;
+							})
+						);
+						const result = await env.MY_PICTURES_BUCKET.list();
+						const list = result.objects.map((e) => ({ key: e.key, size: e.size }));
+						return Response.json({ result: true, message: 'Upload Success!', list }, { status: 200 });
+					}
+					break;
+
+				case 'DELETE':
+					await env.MY_PICTURES_BUCKET.delete(key);
+					return new Response('Delete Success!', { status: 200 });
+
+				default:
+					break;
+			}
+		}
+
+		return new Response('Not Found', { status: 404 });
 	},
 	async scheduled(controller: ScheduledController, env: Env, ctx: ExecutionContext) {
 		ctx.waitUntil(triggerHandler(controller, env));
